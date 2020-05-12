3084627cee476c374c864f453e95508e69e9bed7
zaldanaraul <raularmandozc12@gmail.com>
2020-05-12 12:59:15 -0400

    added tailwind css, postcss, autoprefixer setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import HelloWorld from "./components/helloWorld";
 import CounterExample from "./components/counterExample";
+import Header from "./components/header";
 
 function App() {
   return (
-    <div className="App">
-      <CounterExample name="Jared" />
+    <div>
+      <Header></Header>
+      <HelloWorld name="Jared" />
     </div>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <header>AppName</header>;
+}
+
+export default Header;
